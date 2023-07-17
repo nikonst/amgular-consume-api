6dd265790cb0510d40285dfcba4791638fe91abe
nikonst <nikonst@otenet.gr>
2023-07-17 11:14:26 +0300

    Add basic omponents

--- userapicataloge/src/app/app-routing.module.ts
+++ userapicataloge/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './components/users/users.component';
+import { UserdetailsComponent } from './components/userdetails/userdetails.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'users', component: UsersComponent},
+  {path: 'userdetails/:uuid', component: UserdetailsComponent},
+  {path: '**', redirectTo: 'users'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- userapicataloge/src/app/app.module.ts
+++ userapicataloge/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
+import { UserdetailsComponent } from './components/userdetails/userdetails.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    UserdetailsComponent
   ],
   imports: [
     BrowserModule,
